d6008c1f927030b1de1ef20dd6437e2bcce0b409
danxark <dancaralonzo@gmail.com>
2023-10-02 08:39:11 -0400

    done

--- scripts/w03-task.js
+++ scripts/w03-task.js
@@ -19,21 +19,21 @@ function addNumbers(){
 document.querySelector('#addNumbers').addEventListener('click', addNumbers)
 
 /* Function Expression - Subtract Numbers */
-function add(number1, number2) {
-    const sum = number1 + number2
-    return sum
+function subtract(subtnumber1, subtnumber2) {
+    const  subt= subtnumber1 - subtnumber2
+    return subt
 }
-function addNumbers(){
-  const addNumber1= Number(document.querySelector('#add1').value)
-  const addNumber2= Number(document.querySelector('#add2').value)
-   const sum = add(addNumber1,addNumber2)
- document.querySelector('#sum').value = sum
-     console.log('FIRST NUMBER', addNumber1)
-     console.log('SECOND NUMBER', addNumber2)
-     console.log('SUM', sum)
+function subtractNumbers(){
+  const subtractNumber1= Number(document.querySelector('#subtract1').value)
+  const subtractNumber2= Number(document.querySelector('#subtract2').value)
+   const subt = subtract(subtractNumber1,subtractNumber2)
+ document.querySelector('#difference').value = subt
+     console.log('FIRST NUMBER', subtractNumber1)
+     console.log('SECOND NUMBER', subtractNumber2)
+     console.log('DIFFERENCE', subt)
      
 }         
-document.querySelector('#addNumbers').addEventListener('click', addNumbers)
+document.querySelector('#subtractNumbers').addEventListener('click', subtractNumbers)
 
 
 /* Arrow Function - Multiply Numbers */
