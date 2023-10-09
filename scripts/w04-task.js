0cabe184944babfe0082d02ae1f1cf8a121a96e1
danxark <dancaralonzo@gmail.com>
2023-10-09 09:41:28 -0400

    done

--- scripts/w04-task.js
+++ scripts/w04-task.js
@@ -6,33 +6,34 @@ let myProfile = {
     name: 'Danny Alonzo',
     photo: 'images/danfoto.png',
 
-favoriteFoods: [
+    favoriteFoods: [
     'Ceviche',
     'encebollado',
     'Arroz con camaron',
     'French fries',
     'Apple Pie'
 ],
-hobbies: [
+     hobbies: [
     'Play Guitar',
     'Play videogames',
     'play Voleyball',
     'Play Soccer'
 ]
-}
+};
 /* Populate Profile Object with placesLive objects */
 myProfile.placesLived.push(
     {
         place: 'Portoviejo, Ec',
         length: '25 years'
     }
-);
+)
 
 
 /* DOM Manipulation - Output */
-document.getElementById('#name').textContent = myProfile.name;
-document.querySelector('#photo').src = myProfile.photo;
-document.querySelector('#name').alt = myProfile.photo;
+document.querySelector('#name').textContent= myProfile.name;
+let photo = document.querySelector('#photo');
+photo.setAttribute('src',myProfile.photo);
+photo.setAttribute('alt',myProfile.name);
 myProfile.favoriteFoods.forEach(food => {
     let li = document.createElement('li');
     li.textContent = food;
@@ -43,10 +44,10 @@ myProfile.hobbies.forEach(hobbie => {
     ul.textContent = hobbie;
     document.querySelector('#hobbies').appendChild(ul);
 })
-myProfile.hobbies.forEach(hobbie => {
-    let ul = document.createElement('ul');
-    ul.textContent = hobbie;
-    document.querySelector('#hobbies').appendChild(ul);
+myProfile.placesLived.forEach(length => {
+    let dd = document.createElement('dd');
+    ul.textContent = length;
+    document.querySelector('#places-lived').appendChild(dd);
 })
 
 
